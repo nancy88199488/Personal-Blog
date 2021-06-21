ca901abc0aa16dbe6d18cf11819ff83f02eee8fd
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-21 07:41:37 +0300

    made changes to main views file

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for,flash,abort
 from . import main
 from .. import db,photos
 from flask_login import login_user,logout_user,login_required,current_user
-from  requests import getQuotes
+from  ..requests import getQuotes
 from .forms import BlogForm,CommentForm,updateProfile,SubscriberForm
 from ..models import Blog,Comment,User,Subscriber
 from ..email import mail_message
